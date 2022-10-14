7a924149d714e28b915be9da04ef67fd19a63ad7
Kult_Bestia <54935746+Danil1024@users.noreply.github.com>
2022-10-14 19:22:13 +0600

    Revert "Serializer"

--- women/serializers.py
+++ women/serializers.py
@@ -2,17 +2,7 @@ from rest_framework import serializers
 from .models import Women
 
 
-class WomenSerializer(serializers.Serializer):
-	title = serializers.CharField(max_length=255)
-	content = serializers.CharField()
-	time_create = serializers.DateTimeField(read_only=True)
-	time_update= serializers.DateTimeField(read_only=True)
-	is_published = serializers.BooleanField(default=True)
-	cat_id = serializers.IntegerField()
-
-
-"""class WomenSerializer(serializers.ModelSerializer): Для авто полей модели сразу ставиться read_only=True
+class WomenSerializer(serializers.ModelSerializer):
 	class Meta:
 		model = Women
-		fields = ('title', 'content', 'time_create', 'time_update', 'is_published', 'cat_id')"""
-
+		fields = ('title', 'cat_id')
