5d4b79749c6deb7999bb965422daac3fb42645b4
Danil1024 <danaiek2003@mail.ru>
2022-10-15 19:23:51 +0600

    CRUD serializer

--- women/urls.py
+++ women/urls.py
@@ -3,5 +3,7 @@ from .views import WomenAPIView
 
 
 urlpatterns = [
-	path('api/v1/womenlist/', WomenAPIView.as_view(), name= 'women_list')
+	path('api/v1/womenlist/', WomenAPIView.as_view(), name= 'women'),
+	path('api/v1/change_women/<int:pk>/', WomenAPIView.as_view()),
+	path('api/v1/delete_women/<int:pk>/', WomenAPIView.as_view())
 ]
