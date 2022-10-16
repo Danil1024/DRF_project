31eb443b1e287d53aeefd84dc0210fe5221b73fd
Danil1024 <danaiek2003@mail.ru>
2022-10-16 15:49:26 +0600

    ViewSet ModelViewSet Routers

--- women/urls.py
+++ women/urls.py
@@ -1,8 +1,12 @@
-from django.urls import path
-from .views import WomenListAPIView, WomenAPIDetailView
+from django.urls import path, include
+from .views import WomenViewSet
+from rest_framework import routers
+
+
+router = routers.SimpleRouter()
+router.register(r'women', WomenViewSet)
 
 
 urlpatterns = [
-	path('api/v1/womenlist/', WomenListAPIView.as_view(), name= 'women'),
-	path('api/v1/women/detail/<int:pk>/', WomenAPIDetailView.as_view()),
+	path('api/v1/', include(router.urls)),
 ]
