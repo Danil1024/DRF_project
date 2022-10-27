5af74496cc5748da427de4556e42dc0cdbb6a447
Danil1024 <danaiek2003@mail.ru>
2022-10-27 15:39:30 +0600

    Djoser

--- women/urls.py
+++ women/urls.py
@@ -1,12 +1,11 @@
-from django.urls import path, include
-from .views import WomenRetrieveUpdateAPIView, WomenRetrieveDestroyAPIView,\
-					WomenListAPIView#, UserCreateAPIView
-
+from django.urls import path, include, re_path
+from .views import WomenRetrieveUpdateAPIView, WomenRetrieveDestroyAPIView, WomenListAPIView
 
 
 urlpatterns = [
 	path('api/v1/women/', WomenListAPIView.as_view()),
 	path('api/v1/women/delete/<int:pk>/', WomenRetrieveDestroyAPIView.as_view()),
 	path('api/v1/women/<int:pk>/', WomenRetrieveUpdateAPIView.as_view()),
-	#path('api/v1/user/', UserCreateAPIView.as_view())
+	re_path('api/v1/auth/', include('djoser.urls')),
+    re_path(r'^auth/', include('djoser.urls.authtoken')),
 ]
--- women/views.py
+++ women/views.py
@@ -3,21 +3,15 @@ from rest_framework.views import APIView
 from rest_framework.response import Response
 from django.forms import model_to_dict
 from .models import Women
-from .serializers import WomenSerializer#, UserSerializer
-#from django.contrib.auth.models import User
-from rest_framework.permissions import IsAuthenticatedOrReadOnly
+from .serializers import WomenSerializer
+from rest_framework.permissions import IsAuthenticated
 from .permissions import IsAdminOrReadOnly, IsOwnerOrReadOnly
-
-
-"""class UserCreateAPIView(generics.CreateAPIView):
-	queryset = User.objects.all()
-	serializer_class = UserSerializer"""
 
 
 class WomenListAPIView(generics.ListCreateAPIView):
 	queryset = Women.objects.all()
 	serializer_class = WomenSerializer
-	permission_classes = (IsAuthenticatedOrReadOnly,)
+	permission_classes = (IsAuthenticated,)
 
 
 class WomenRetrieveUpdateAPIView(generics.RetrieveUpdateAPIView):
@@ -30,43 +24,3 @@ class WomenRetrieveDestroyAPIView(generics.RetrieveDestroyAPIView):
 	queryset = Women.objects.all()
 	serializer_class = WomenSerializer
 	permission_classes = (IsAdminOrReadOnly,)
-
-
-
-
-"""
-class WomenAPIView(APIView):
-	def get (self, request):
-		list_women = Women.objects.all()
-		return Response({'list_women': WomenSerializer(list_women, many=True).data})
-
-	def post(self, request):
-		serializer = WomenSerializer(data= request.data)
-		serializer.is_valid(raise_exception= True)
-		serializer.save()
-		return Response({'new_women': serializer.data})
-
-	def put(self, request, *args, **kwargs):
-		pk = kwargs.get('pk', None)
-		if not pk:
-			return Response({'Error':'Method PUT not allowed'})
-		try:
-			instance = Women.objects.get(pk=pk)
-		except:
-			return Response({'Error':'Object does not exist'}
-		serializer = WomenSerializer(data= request.data, instance=instance)
-		serializer.is_valid(raise_exception= True)
-		serializer.save()
-		return Response({'change_women':serializer.data})
-
-	def delete(self, request, *args, **kwargs):
-		pk = kwargs.get('pk', None)
-		if not pk:
-			return Response({'Error':'Method PUT not allowed'})
-		try:
-			women = Women.objects.get(pk=pk)
-		except:
-			return Response({'Error':'Object does not exist'})
-		women.delete()
-		return Response({'delete_women_id':pk,'delete_women': model_to_dict(women)})
-"""
