ca68751c445ecfc15034fe2b7c13d2ea4134cecf
Danil1024 <danaiek2003@mail.ru>
2022-10-28 15:36:30 +0600

    added filter

--- women/views.py
+++ women/views.py
@@ -1,17 +1,16 @@
-from rest_framework import generics, viewsets
-from rest_framework.views import APIView
-from rest_framework.response import Response
-from django.forms import model_to_dict
+from rest_framework import generics
 from .models import Women
 from .serializers import WomenSerializer
 from rest_framework.permissions import IsAuthenticated
 from .permissions import IsAdminOrReadOnly, IsOwnerOrReadOnly
+from .filters import IsOwnerOrAdminFilterBackend
 
 
 class WomenListAPIView(generics.ListCreateAPIView):
 	queryset = Women.objects.all()
 	serializer_class = WomenSerializer
 	permission_classes = (IsAuthenticated,)
+	filter_backends = [IsOwnerOrAdminFilterBackend,]
 
 
 class WomenRetrieveUpdateAPIView(generics.RetrieveUpdateAPIView):
